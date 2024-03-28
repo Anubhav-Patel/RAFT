0be327ecf52a01847da3a099ad4d414b2dfeed2b
Anubhav\-Patel <anubhavpatel099@gmail.com>
2024-03-28 12:47:44 +0530

    added functions

--- node.py
+++ node.py
@@ -4,8 +4,11 @@ import raft_pb2
 import raft_pb2_grpc
 import os
 import time
+import random
+import threading
+    
 
-class Node:
+class RaftService(raft_pb2_grpc.RaftServiceServicer):
     def __init__(self, node_id):
         self.node_id = node_id
         self.current_term = 0
@@ -15,98 +18,92 @@ class Node:
         self.current_role = "follower"
         self.current_leader = None
         self.votes_received = set()
-        self.other_nodes = {1:0, 2:0, 3:0, 4:0, 5:0}
+        self.other_nodes = [50051, 50052, 50053, 50054, 50055]
         self.address = None
         self.election_timeout = 10
-        self.election_period_ms = randint(1000, 5000)
+        self.election_period_ms = random.randint(1000, 5000)
         self.sent_length = {1:0, 2:0, 3:0, 4:0, 5:0}
         self.acked_length = {1:0, 2:0, 3:0, 4:0, 5:0}
-
-
-class RaftService(rat_pb2_grpc.RaftServiceServicer):
-    int node_id = int(input("Enter Node ID: "))
-    node1 = Node(node_id)
-
-    def recover():
-        if not os.path.exists(self.log_file):
-            with open(self.log_file, "w") as log_file:
-                pass
-        if not os.path.exists(self.meta_file):
-            with open(self.meta_file, "w") as meta_file:
-                meta_file.write(f"node_id: {self.node_id}\n")
-                meta_file.write(f"currTerm: {self.currTerm}\n")
-                meta_file.write(f"votedFor: {self.votedFor}\n")
-                meta_file.write(f"commitLength: {self.commitLength}\n")
-
-    def start_election_timer(self, request, context):
-        while self.election_timeout > 0:
-            time.sleep(1)
-            self.election_timeout -= 1
-            if self.election_timeout <= 0:
+        thread = threading.Thread(target=self.start_election_timer)
+        thread.start()
+        # self.start_election_timer()
+        
+    def start_election_timer(self):
+        i = 1
+        while(i >= 0):
+            if(i == 0):
                 self.start_election()
-                break
+                print(self.current_role)
+                i = 1
+            time.sleep(1)
+            i = i-1
 
-    def cancel_election_timer():
-        self.election_timeout = 0
 
-    def RequestVote(self, request, context):
+    def start_election(self):
         self.current_term = self.current_term + 1
         self.current_role = "candidate"
         self.voted_for = self.node_id
-        self.votes_received = self.votes_received.add(self.node_id)
-
-        if self.log:
-            last_term = self.log[-1]['term']
-        else:
-            last_term = 0
-        
-        vote_request_msg = ("VoteRequest", self.node_id, self.current_term, len(self.log), last_term)
-        
-        for node in self.other_nodes:
-            node_pb2.RequestVote(term = self.current_term, candidate_id = self.)
-
-        self.start_election_timer()        
-
-
-
-    def giveVote(self, request, context):
-        if(request.current_term > self.current_term):
-            self.current_term = request.current_term
-            self.current_role = "follower"
+        self.votes_received.add(self.node_id)
         
         last_term = 0
-        if self.log:
-            last_term = self.log[-1].term
+        if len(self.log) > 0:
+            self.last_term = self.log[len(self.log) - 1].term
+        
+        try:
+            for nodes in self.other_nodes:
+                if nodes != self.node_id:
 
-        if request.log:
-            candidate_log_term = request.log[-1].term
+                    # print("xxx")
+                    channel = grpc.insecure_channel(f'localhost:{nodes}')  
+                    stub = raft_pb2_grpc.RaftServiceStub(channel)
+                    vote_request = raft_pb2.RequestVoteRequest(term = self.current_term, candidate_id = self.node_id, last_log_index = len(self.log), last_log_term = last_term)
+                    response = stub.RequestVote(vote_request)
+                    time.sleep(1)
+                    print(response.vote_granted)
+                    self.CollectVote(response)
+                    # if(response.vote_granted == True):
+                    #     self.votes_received.add(nodes.node_id)
+        except:
+            print("Err")
+            
+        # start_election_timer(self)
 
-        log_ok = (candidate_log_term > last_term) or (candidate_log_term == last_term and len(request.log) >= len(self.log))
 
-        if request.current_term == self.current_term and log_ok and (self.voted_for in {request.node_id, None}):  #if voted for the candidate in the same term
-            self.voted_for = candidate_id
-            #TODO send vote
+    def RequestVote(self, request, context):
+        print(request.term)
+        print(self.current_term)
+        if request.term > self.current_term:
+            self.current_term = request.term
+            self.voted_for = None
+            self.current_role = "Follower"
+            self.current_leader = None
+        if request.term < self.current_term:
+            
+            return raft_pb2.RequestVoteResponse(term=self.current_term, vote_granted=False)
+        if self.voted_for is None or self.voted_for == request.candidate_id:
+            self.voted_for = request.candidate_id
+            self.votes_received.add(request.candidate_id)
+            return raft_pb2.RequestVoteResponse(term=self.current_term, vote_granted=True)
+        return raft_pb2.RequestVoteResponse(term=self.current_term, vote_granted=False)
 
 
-
-    def CollectVote(self, request, context):
-        if self.current_role == "candidate" and request.term == self.current_term and request.granted:
-            self.votes_received.add(request.node_id)
+    def CollectVote(self, response):
+        if self.current_role == "candidate" and response.term == self.current_term and response.vote_granted:
             if len(self.votes_received) >= (len(self.other_nodes) + 1) // 2:
                 self.current_role = "leader"
                 self.current_leader = self.node_id
-                self.cancel_election_timer()
+                # self.cancel_election_timer()
                 for follower_id in self.other_nodes:
                     if follower_id != self.node_id:
                         self.sent_length[follower_id] = len(self.log)
                         self.acked_length[follower_id] = 0
-                        self.replicate_log(follower_id)
+                        self.ReplicateLog(follower_id)
 
-        elif term > self.current_term:
-            self.current_term = term
+        elif response.term > self.current_term:
+            self.current_term = response.term
             self.current_role = "follower"
             self.voted_for = None
-            self.cancel_election_timer()
+            # self.cancel_election_timer()
 
 
     def broadcast_msg_to_followers(self, request, context):
@@ -115,28 +112,32 @@ class RaftService(rat_pb2_grpc.RaftServiceServicer):
             self.acked_length[self.node_id] = len(self.log)
             for follower_id in self.other_nodes:
                 if follower_id != self.node_id:
-                    self.replicate_log(follower_id)
-        else:
-            if self.current_leader is not None:
+                    self.ReplicateLog(follower_id)
+        # else:
+        #     if self.current_leader is not None:
                 # self.forward_to_leader(msg)
 
 
-    def replicate_log(follower_id):
+    def ReplicateLog(self, follower_id):
         previous_len = self.sent_length.get(follower_id, 0) 
         suffix = self.log[previous_len:]
         previous_term = 0
         if previous_len > 0:
             previous_term = self.log[previous_len - 1]['term']
         
-        log_request_msg = (self.log['msg'], self.node_id, self.current_term, previous_len, previous_term, self.commit_length, suffix)
-        self.send_message(log_request_msg, follower_id)
+        if(len(self.log) == 0):
+            last_log_msg = ""
+        else:
+            last_log_msg = self.log[-1]['msg']
+        # log_request_msg = raft_pb2.AppendEntries(term = self.current_term, leader_id = self.node_id, prev_log_index = len(self.log)-1, prev_log_term = previous_term, suffix, leader_commit = self.leader_commit)    
+        
 
 
-    def receive_msg(self, request, context):
+    def logRequest(self, request, context):
         if request.term > self.current_term:
             self.current_term = request.term
             self.voted_for = None
-            self.cancel_election_timer()
+            #TODO self.cancel_election_timer()
 
         if request.term == self.current_term:
             self.current_role = "follower"
@@ -185,7 +186,7 @@ class RaftService(rat_pb2_grpc.RaftServiceServicer):
                 self.commit_log_entries()
             elif self.sent_length.get(follower, 0) > 0:
                 self.sent_length[follower] -= 1
-                self.replicate_log(self.node_id, follower)
+                self.ReplicateLog(self.node_id, follower)
         elif term > self.current_term:
             self.current_term = term
             self.current_role = "follower"
@@ -205,24 +206,24 @@ class RaftService(rat_pb2_grpc.RaftServiceServicer):
                 self.commit_log_entries()
             elif self.sent_length.get(follower, 0) > 0:
                 self.sent_length[follower] -= 1
-                self.replicate_log(self.node_id, follower)
+                self.ReplicateLog(self.node_id, follower)
         elif term > self.current_term:
             self.current_term = term
             self.current_role = "follower"
             self.voted_for = None
             self.cancel_election_timer()
+    
 
+if __name__ == '__main__':
+    node_id = int(input("Enter Node ID: "))
 
-
-
-
-def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    raft_pb2_grpc.add_RaftServiceServicer_to_server(RaftService(), server)
-    server.add_insecure_port('[::]:50051')
+    raft_pb2_grpc.add_RaftServiceServicer_to_server(RaftService(node_id), server)
+    server.add_insecure_port(f'[::]:{node_id}')
     server.start()
     print("Node1 server started. Listening on port 50051.")
     server.wait_for_termination()
 
-if __name__ == '__main__':
-    serve()
+    # while True:
+    #     print(f"Node {node_id} - Role: {raft_node.current_role}, Term: {raft_node.current_term}")
+    #     time.sleep(5)
